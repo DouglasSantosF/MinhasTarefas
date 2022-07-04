8cfa1ccd821c39e85e5d130066402eca3c5fbd2c
Douglas dos Santos <doug.frutuoso@gmail.com>
2022-07-04 15:41:59 -0300

    seed configurado

---
+++ minhasTarefas/app/backend/database/seeders/20220704183019-tarefas.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-empty-function */
+/* eslint-disable no-unused-vars */
+module.exports = {
+  up: async (queryInterface, _Sequelize) => queryInterface.bulkInsert(
+    'tarefas',
+    [
+      {
+        tarefa: 'Ir para a academia',
+        status: 'pendente',
+      },
+      {
+        tarefa: 'Ir ao mercado',
+        status: 'em andamento',
+      },
+      {
+        tarefa: 'Jogar o lixo fora',
+        status: 'pronto',
+      },
+    ],
+    {},
+  ),
+
+  async down(queryInterface, Sequelize) {
+
+  },
+};
