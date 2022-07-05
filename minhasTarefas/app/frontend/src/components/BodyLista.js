92acd7d8d8e5795a1400ac8f670bfb78e748baea
Douglas dos Santos <doug.frutuoso@gmail.com>
2022-07-05 14:13:06 -0300

    corrigindo erro de loading

--- minhasTarefas/app/frontend/src/components/BodyLista.js
+++ minhasTarefas/app/frontend/src/components/BodyLista.js
@@ -1,4 +1,4 @@
-import {React, useContext} from 'react';
+import {React, useContext, useEffect} from 'react';
 import listaContext from '../context';
 
 import ListaDeTarefas from "./ListaDeTarefas";
@@ -6,25 +6,30 @@ import '../css/BodyLista.css'
 
 const BodyLista = () => {
 
-  const { tarefasApi } = useContext(listaContext);
+  const { tarefasApi, loading, setLoading } = useContext(listaContext);
 
-  console.log(tarefasApi);
+  useEffect( () => {
+    setLoading(false)
+  },[tarefasApi])
+
+  console.log(loading);
 
   const mapList = () => {
-
-   const render = tarefasApi.map((item, ind) =>
-    <ListaDeTarefas key={ind} tarefa={item.tarefa }status={item.status} id={item.id} />
-   )
+    const render = tarefasApi.map((item, ind) => 
+      <ListaDeTarefas key={ind} tarefa={item.tarefa }status={item.status} id={item.id} />
+    )
     return render;
-
   }
 
-  return(
+  const Loading = <h1> LOADING </h1>;
+
+  return( loading? Loading : (
     <div className="bodyListaContainer">
       <div className="bodyItens">
-          {mapList}
+        {mapList()}
       </div>
     </div>
+      )
   );
 
 
